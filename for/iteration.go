48eb3303781f11daa5d35b7fa1904fe1c8a2f099
katebry <katebryan31@gmail.com>
2021-12-16 20:37:02 -0800

    chore: added return type

--- for/iteration.go
+++ for/iteration.go
@@ -3,6 +3,6 @@ package iteration
 import "strings"
 
 func Repeat(character string, repeatCount int) string {
-	var repeated = strings.Repeat(character, repeatCount)
+	var repeated string = strings.Repeat(character, repeatCount)
 	return repeated
 }
