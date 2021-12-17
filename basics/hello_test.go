140d772325bc24ea982f758df75287e628912469
katebry <katebryan31@gmail.com>
2021-12-16 19:23:18 -0800

    feat: final refactor of Hello function

--- basics/hello_test.go
+++ basics/hello_test.go
@@ -27,4 +27,10 @@ func TestHello(t *testing.T) {
 		want := "Hola, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Kate", "French")
+		want := "Bonjour, Kate"
+		assertCorrectMessage(t, got, want)
+	})
 }
