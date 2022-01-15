9149f5b71305cad936f18c71db18a6580a7ea7ba
katebry <katebryan31@gmail.com>
2022-01-15 12:46:51 -0800

    feat: added Withdraw function and test refactor

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,13 +1,23 @@
 package wallet
 
+import "fmt"
+
 type Bitcoin int
 
 type Wallet struct {
 	balance Bitcoin
 }
 
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
 func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
 }
 
 func (w *Wallet) Balance() Bitcoin {
